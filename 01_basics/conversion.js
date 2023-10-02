12d27899208303296778079638e2c8dc8b8acaed
prathmesh1709 <79852686+prathmesh1709@users.noreply.github.com>
2023-10-02 10:23:42 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,21 @@
+let age = undefined
+
+let convertToString = Number(age)
+
+console.log(typeof convertToString);
+console.log(convertToString);
+
+//33 => 33
+//33an => NaN
+// undefined => undefined
+
+let loggedIn = 1
+let checkLoggedIn = Boolean(loggedIn)
+
+console.log(typeof checkLoggedIn)
+console.log(checkLoggedIn);
+
+// 1 => true
+// true => true
+// "" => false
+// "te" => true
